f84114fc16301ee3b47e7df699797f10030633d7
Matheus H. A. de Francisco <matheushenrique612@gmail.com>
2024-04-24 22:20:47 -0300

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,12 @@
+import styles from './StylesModules/Footer.module.css'
+import { FaSquareGithub, FaSquareWhatsapp, FaLinkedin, FaSquareInstagram } from "react-icons/fa6";
+import { IoMdMail } from "react-icons/io";
+
 function Footer() {
-    return(
-        <h1>Footer</h1>
+    return (
+        <footer>
+            <p>© Matheus HAF - Todos direitos reservado</p>
+        </footer>
     )
 }
 export default Footer
--- src/components/NavbarMenu.js
+++ src/components/NavbarMenu.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Styles from './StylesModules/Navbar.module.css'
-
+import logo from '../images/logo.png'
 function NavbarMenu() {
     return(
         <header>
-            <div className={Styles.right}></div>
+            <div className={Styles.right}><img src={logo} alt="logo" /></div>
             <div className={Styles.left}>
                 <ul>
                     <li><a href="#">Inicio</a></li>
