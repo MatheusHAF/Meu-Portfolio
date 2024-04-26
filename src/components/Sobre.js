37e6bdd496f88f5ecb5e3081a6030ae0c2a49c14
Matheus H. A. de Francisco <matheushenrique612@gmail.com>
2024-04-26 20:52:06 -0300

    .

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -1,6 +1,6 @@
 import styles from './StylesModules/Sobre.module.css'
 
-import { FaGithub,FaPython,FaReact, FaHtml5,FaCss3Alt,  } from "react-icons/fa";
+import { FaGithub,FaPython,FaReact, FaHtml5,FaPhp , FaGitAlt  } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
 import { BiLogoCPlusPlus } from "react-icons/bi";
 
@@ -8,12 +8,19 @@ function Sobre() {
     return(
         <div id="about" className={styles.container}>
             <h1>Um pouco sobre mim</h1>
-            <p>Exercitation in anim adipisicing aliqua velit et Lorem fugiat nulla ipsum veniam consequat pariatur Lorem. Ex aute veniam cillum aute tempor cupidatat tempor in ex. Laboris irure minim consectetur ad ipsum enim. Aliquip consectetur excepteur duis ea pariatur ex commodo incididunt tempor id consequat occaecat.</p>
+            <p>
+                Olá, Sou o Matheus e tenho 23 anos. Atualmente sou granduando em Ciência Computação pela Universidade Estadual de Campinas (Unicamp) e já me formei em Técnico em Informática em 2019 pelo IFSP, onde me apaixonei por programação.
+                
+            </p>
             <h2>Tecnologias que ja utilizei</h2>
             <div className={styles.tech}>
                 <nav>
                     <FaPython/>
                     <p>Python</p>
+                </nav>
+                <nav>
+                    <FaGitAlt/>
+                    <p>Git</p>
                 </nav>
                 <nav>
                     <FaReact/>
@@ -28,8 +35,8 @@ function Sobre() {
                     <p>HTML5</p>
                 </nav>
                 <nav>
-                    <FaCss3Alt/>
-                    <p>CSS</p>
+                    <FaGithub/>
+                    <p>Github</p>
                 </nav>
                 <nav>
                     <IoLogoJavascript/>
