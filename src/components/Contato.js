94e70f15137611e0dc7e4c6f4c9d27d6c604d50f
Matheus H. A. de Francisco <matheushenrique612@gmail.com>
2024-04-26 19:56:28 -0300

    .

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -1,10 +1,10 @@
 import styles from './StylesModules/Contato.module.css';
 import Forms from './Forms';
-import React, {useState} from 'react'
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import React, { useState } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 
-import { FaSquareGithub, FaSquareWhatsapp, FaLinkedin, FaSquareInstagram  } from "react-icons/fa6";
+import { FaSquareGithub, FaSquareWhatsapp, FaLinkedin, FaSquareInstagram } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
 import { FaRegCopy } from "react-icons/fa";
 
@@ -17,30 +17,35 @@ function Contato() {
                 <div className={styles.left}>
                     <h2>Minhas redes sociais</h2>
                     <nav>
-                        <FaSquareWhatsapp/>
-                        <p>Whatsapp</p>
-                        <div className={styles.copy}><CopyToClipboard text='+55(16)98848-1389'><FaRegCopy/></CopyToClipboard> </div>
+                        
+                        <a href="#contacts">
+                            <FaSquareWhatsapp />
+                            <p>Whatsapp</p>
+                            <div className={styles.copy}><CopyToClipboard text='+55(16)98848-1389'><FaRegCopy /></CopyToClipboard> </div>
+                        </a>
                     </nav>
                     <nav>
-                        <IoMdMail/>
-                        <p>E-mail</p>
-                        <div className={styles.copy}><CopyToClipboard text='matheushenrique612@gmail.com'><FaRegCopy/></CopyToClipboard></div>
+                        <a href="#contacts">
+                            <IoMdMail />
+                            <p>E-mail</p>
+                            <div className={styles.copy}><CopyToClipboard text='matheushenrique612@gmail.com'><FaRegCopy /></CopyToClipboard></div>
+                        </a>
                     </nav>
                     <nav>
-                        <FaLinkedin/>
-                        <p>Linkedin</p>
+                        <a href="https://www.linkedin.com/in/matheus-francisco-a5bb1a218/" target='_blank'>
+                            <FaLinkedin />
+                            <p>Linkedin</p>
+                        </a>
                     </nav>
                     <nav>
-                        <FaSquareGithub/>
-                        <p>Github</p>
-                    </nav>
-                    <nav>
-                        <FaSquareInstagram/>
-                        <p>Instagram</p>
+                        <a href="https://github.com/MatheusHAF" target='_blank'>
+                            <FaSquareGithub />
+                            <p>Github</p>
+                        </a>
                     </nav>
                 </div>
                 <div className={styles.right}>
-                    <Forms/>
+                    <Forms />
                 </div>
             </div>
         </div>
